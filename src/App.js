f09f1bf2b6dc5d9342b5e4292f6354237f16a691
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-06-09 21:33:41 +0700

    integrated firebase and add button sign in with google

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.components.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignUpIn from './pages/signupin/signupin.component.jsx'
 import Header from './components/header/header.component.jsx'
-import {auth} from './firebase/firebase.utils.jsx'
+import {auth} from './firebase/firebase.utils.js'
 
 class App extends React.Component {
   constructor(){
@@ -17,19 +17,24 @@ class App extends React.Component {
     }
   }
 
+  unsubsribeFromAuth = null
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubsribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({currentUser: user})
 
-      
     })
+  }
+
+  componentWillUnmount(){
+    this.unsubsribeFromAuth();
   }
 
   render(){
     return (
       <div>
         <BrowserRouter>
-          <Header />
+          <Header currentUser={this.state.currentUser}/>
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
