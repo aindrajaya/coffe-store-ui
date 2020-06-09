befda79c4d3f9ef7a41337b9f7f15344e9c1d9b6
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-06-09 16:05:49 +0700

    this is the first layout setup

---
+++ src/homepage.components.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className='directory-menu'>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'>MOBIL</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'>HUUU</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'>YUHUU</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className='title'>JACKET</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
